ee9f2914965c6385d8086d1c768668c49a7fc50e
Garfield-Zhang <garfield.zhang20@imperial.ac.uk>
2022-11-08 17:22:53 +0000

    I COMMIT

--- src/main/java/Servlet.java
+++ src/main/java/Servlet.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Servlet {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns ={ "/patients","/doctors","/programming"},loadOnStartup = 1)
+public class Servlet extends HttpServlet {
+
+    @Override
+    protected void doGet(javax.servlet.http.HttpServletRequest req, javax.servlet.http.HttpServletResponse resp) throws javax.servlet.ServletException, java.io.IOException {
+        resp.getWriter().println(req.getServletPath()+" is fun (KILL ME)");
+    }
+    @Override
+    protected void doPost(javax.servlet.http.HttpServletRequest req, javax.servlet.http.HttpServletResponse resp) throws javax.servlet.ServletException, java.io.IOException {
+        resp.getWriter().println("Meow Meow");
+    }
 }
+
